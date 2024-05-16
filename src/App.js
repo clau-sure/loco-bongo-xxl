d93ed8dc9c3e44f1c63b4ce75862156d6e900f2f
Claudia Álvarez <claudia.alvarez.arias@gmail.com>
2024-05-16 18:19:54 +0200

    dani's changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import "./App.scss";
 import "./assets/Noah Text Heavy.ttf"
 import logo from './assets/logo.svg'
 
 
 function App() {
+  const [prevNumber, setPrevNumber] = useState(null);
   const number = (num) => {
     return (
       <div key={num} id={num} className="number">
-        <p>{num}</p>
+        <p id={'p_' + num}>{num}</p>
       </div>
     );
   };
@@ -20,20 +22,29 @@ function App() {
     return rows;
   };
 
-  const enterAnadir = (event) => {
+  const enterAdd = (event) => {
     if (event.key === 'Enter') {
       enterNumber();
     }
   }
 
   const enterNumber = () => {
-    let inputValue = document.getElementById("anadir").value;
-    let classList = document.getElementById(inputValue).classList;
-    if (classList.contains('flip')) {
-      document.getElementById(inputValue).classList.remove('flip');
-    } else {
-      document.getElementById(inputValue).classList.add('flip');
+    let inputValue = document.getElementById("add").value;
+    if (inputValue > 0 && inputValue <= 90) {
+      if (prevNumber) {
+        document.getElementById(prevNumber).classList.remove('heartbeat');
+      }
+      document.getElementById('p_' + inputValue).classList.add('heartbeat');
+      setPrevNumber('p_' + inputValue);
+      let classList = document.getElementById(inputValue).classList;
+      if (classList.contains('flip')) {
+        document.getElementById(inputValue).classList.remove('flip');
+        document.getElementById('p_' + inputValue).classList.remove('heartbeat');
+      } else {
+        document.getElementById(inputValue).classList.add('flip');
+      }
     }
+    document.getElementById("add").value = null;
   };
 
   return (
@@ -44,11 +55,11 @@ function App() {
         <div className="inputdiv">
           <input
             type="number"
-            id="anadir"
+            id="add"
             min="1"
             max="90"
-            defaultValue="0"
-            onKeyDown={enterAnadir}
+            defaultValue="null"
+            onKeyDown={enterAdd}
           ></input>
         </div>
       </div>
